c1a077bc95fdf9563f21bb259765e5c5d5397686
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-21 21:57:51 +0200

    Changed DaisyUi theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  daisyui: {
+    themes: ["light"],
+  },
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        dark: "#464a6d",
-        primary: "#24c97f",
       },
     },
   },
