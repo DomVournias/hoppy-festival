bd6867300d02ba229fcde5fdc65552230afa0cf6
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-21 21:56:19 +0200

    Minor updates

--- app/components/sections/Hero.jsx
+++ app/components/sections/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = ({ section }) => {
         <div className="absolute z-10 space-y-2 text-white text-center">
           <div
             dangerouslySetInnerHTML={{
-              __html: section.hero.heading,
+              __html: section.heading,
             }}
             className="text-6xl font-extrabold pb-4 "
           />
           <div
             dangerouslySetInnerHTML={{
-              __html: section.hero.description,
+              __html: section.description,
             }}
             className="text-lg font-semibold"
           />
--- app/components/timer/ExpiredNotice.jsx
+++ app/components/timer/ExpiredNotice.jsx
@@ -4,7 +4,6 @@ const ExpiredNotice = () => {
       <span className="text-2xl text-white font-semibold">
         To Festival ολοκληρώθηκε!
       </span>
-      {/* <p>Please select a future date and time.</p> */}
     </div>
   );
 };
