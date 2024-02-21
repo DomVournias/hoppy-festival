3cd22b17b7ad93cfc73ad26c93daa81bafa7d7e6
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-21 21:57:27 +0200

    Fixed Counter - Dynamic Import

--- app/components/sections/Upcoming.jsx
+++ app/components/sections/Upcoming.jsx
@@ -1,24 +1,31 @@
-import CountdownTimer from "../timer/CountdownTimer";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const CountdownTimer = dynamic(() => import("../timer/CountdownTimer"), {
+  ssr: false,
+  loading: () => (
+    <span className="loading loading-infinity loading-lg text-accent-content"></span>
+  ),
+});
 
 const Upcoming = ({ section }) => {
-  const date = section.upcoming.eventDate;
+  const date = section.eventDate;
 
   return (
     <section>
-      <div className="w-full bg-primary">
+      <div className="w-full bg-accent ">
         <div className="flex max-w-7xl m-auto items-center justify-between py-6">
-          <div className="text-white space-y-4">
+          <div className="text-accent-content space-y-4">
             <div
-              dangerouslySetInnerHTML={{ __html: section.upcoming.heading }}
-              className="text-4xl font-bold"
+              dangerouslySetInnerHTML={{ __html: section.heading }}
+              className="text-4xl font-bold "
             />
             <div
               className="text-lg font-medium"
-              dangerouslySetInnerHTML={{ __html: section.upcoming.description }}
+              dangerouslySetInnerHTML={{ __html: section.description }}
             />
           </div>
-          <div>
+          <div className="flex items-center justify-center h-36">
             <CountdownTimer targetDate={date} />
           </div>
         </div>
