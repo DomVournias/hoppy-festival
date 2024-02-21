fdb7d3eca78f498f772fa04c759682af635a708c
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-21 21:54:23 +0200

    Added Carousel

--- app/components/carousel/Carousel.jsx
+++ app/components/carousel/Carousel.jsx
@@ -1,9 +1,39 @@
+import Image from "next/image";
 import React from "react";
 
-const Carousel = () => {
+const Carousel = ({ items }) => {
   return (
-    <div>
-      <h2>s</h2>
+    <div className="carousel w-full relative h-screen">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          id={`image-${index}`}
+          className="carousel-item relative w-full"
+        >
+          <Image
+            alt={item.title}
+            src={item.sourceUrl}
+            className="w-full"
+            quality={100}
+            fill={true}
+            style={{ objectFit: "cover" }}
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a
+              href={`#image-${index === 0 ? items.length - 1 : index - 1}`}
+              className="btn btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href={`#image-${(index + 1) % items.length}`}
+              className="btn btn-circle"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
