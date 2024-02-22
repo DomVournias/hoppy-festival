0c217d936919a4b38f021988ec40b5c727df5b07
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-22 20:32:05 +0200

    Added Glass Effect - Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,7 +11,10 @@ const Header = async () => {
   const menu = res.data.menu.menuItems.nodes;
 
   return (
-    <header className="absolute z-50 w-full">
+    <header
+      className="absolute z-50 w-full bg-gray-600 backdrop-filter backdrop-blur-md bg-opacity-20 border-b-[0.2px] border-[rgba(255,255,255,0.2)]
+    "
+    >
       <div className="flex justify-between items-center max-w-7xl m-auto h-20 z-50 text-white">
         <Link href="/" className="text-lg font-bold flex items-center gap-3">
           <Image
@@ -27,7 +30,7 @@ const Header = async () => {
           <ul className="flex gap-4">
             {menu.map((item) => (
               <li key={item.id}>
-                <Link href={item.url} className="p-4 block text-sm font-medium">
+                <Link href={item.url} className="p-4 block text-sm font-bold">
                   {item.label}
                 </Link>
               </li>
