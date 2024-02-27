57d2a528889bdec2ea58674bd9d5b8189dbae654
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-27 21:41:39 +0200

    Info Responsive

--- app/components/sections/Info.jsx
+++ app/components/sections/Info.jsx
@@ -4,12 +4,10 @@ import React from "react";
 const Info = ({ section }) => {
   // console.log(section.info);
   return (
-    <section id="info" className="py-10 bg-accent sm:py-16 lg:py-40">
-      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+    <section id="info" className="py-10 bg-accent paddings">
+      <div className="px-4 mx-auto max-w-7xl">
         <div className="max-w-2xl mx-auto text-center">
-          <h2 className="text-4xl font-bold pb-2 text-accent-content">
-            {section.heading}
-          </h2>
+          <h2 className="heading text-accent-content">{section.heading}</h2>
           <div
             className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-accent-content"
             dangerouslySetInnerHTML={{ __html: section.description }}
