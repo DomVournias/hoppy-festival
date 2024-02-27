e227d37ddc4facb3ee144c8e3de56cd22ddb9572
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-27 21:41:28 +0200

    Actions Responsive

--- app/components/sections/Actions.jsx
+++ app/components/sections/Actions.jsx
@@ -5,12 +5,10 @@ const Actions = ({ section }) => {
   // console.log(section);
   return (
     <section id="actions">
-      <div className="flex space-x-20 items-center h-screen relative">
+      <div className="relative flex flex-col-reverse gap-10 xl:gap-0 xl:flex-row xl:space-x-20 items-center xl:h-screen">
         <Carousel items={section.gallery.nodes} />
-        <div className="w-1/2 pr-40 space-y-6 flex flex-col justify-center py-40">
-          <h3 className="text-4xl font-bold pb-2 text-black ">
-            {section.heading}
-          </h3>
+        <div className="w-full xl:w-1/2 xl:pr-40 space-y-6 flex flex-col justify-center md:py-40 px-4">
+          <h3 className="heading md:pb-2">{section.heading}</h3>
           <div dangerouslySetInnerHTML={{ __html: section.description }} />
         </div>
       </div>
