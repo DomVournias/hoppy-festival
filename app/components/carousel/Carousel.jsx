ff6b32f7241b01e159527951528d48f82fc92d8d
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-02-29 14:52:53 +0200

    Update Carousel.jsx

--- app/components/carousel/Carousel.jsx
+++ app/components/carousel/Carousel.jsx
@@ -14,7 +14,7 @@ const Carousel = ({ items }) => {
             alt={item.title}
             src={item.sourceUrl}
             className="w-full"
-            sizes={item.srcSet}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             quality={100}
             fill={true}
             style={{ objectFit: "cover" }}
